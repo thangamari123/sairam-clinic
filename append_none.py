df2720e986ed84efe606c6e277e6cd295f15cf66
Thanga Mari <thangamari287@gmail.com>
2026-06-12 23:37:53 +0530

    remove temporary scripts

--- append_none.py
+++
@@ -1,7 +0,0 @@
-import os
-
-css_path = r"c:\Users\thang\OneDrive\Desktop\sairam-clinic\style.css"
-with open(css_path, "a", encoding="utf-8") as f:
-    f.write("\n.mobile-bottom-nav { display: none; }\n")
-
-print("Appended successfully.")
